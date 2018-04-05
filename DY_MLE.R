d80a2ce20f54018a57166ef84c0c21cb7992094d
mindy2801 <35322376+mindy2801@users.noreply.github.com>
2018-04-05 11:28:51 +0900

    Add files via upload

--- DY_MLE.R
+++ DY_MLE.R
@@ -12,45 +12,82 @@
 
 
 #For baseline model
-rm(list=ls(all=TRUE))
-source("probabiliry.R")
+rm(list=ls())
+source("probability.R")
 
-datname="choices correct Bechara SDIandcontrols.txt"
+datname="WCST Sample data.txt"
 rawdatamat=read.table(datname,encoding="UTF-8")  
 subjlabels=rawdatamat[,257]				#reads extra information in datafile if available
 subjgroup= rawdatamat[,258]
 
-maxiter <- 100
-maxsubj <- nrow[rawdatamat]
+maxiter <- 10
+maxsubj <- nrow(rawdatamat)
 
 parbounds=c(0,0,.01,.01,1,1,5,5)  #boundaries for r, p, d, i
   lb=parbounds[1:4]
   ub=parbounds[5:8]
 
+freeparsmat=matrix(nrow=24,ncol=4,dimnames=list(NULL,c("r","p","d","i")))  #setting up a matrix that lists the free pars of each of 24 models
+fixedvalsmat=matrix(-1,nrow=24,ncol=4,dimnames=list(NULL,c("r","p","d","i"))) #parameter constraint values of 24 models
+pequalsrmat=matrix(0,nrow=24,ncol=1)
+  for (ploop in 0:1) for (dloop in 0:1) for (iloop in 0:2) for (rloop in 0:1)
+  {
+    rowloop=ploop*12 + dloop*6 + iloop*2 + rloop +1
+    freeparsmat[rowloop,1]=(if(rloop==0) "r" else "")
+    freeparsmat[rowloop,2]=(if(ploop==0) "p" else "")
+    freeparsmat[rowloop,3]=(if(dloop==0) "d" else "")
+    freeparsmat[rowloop,4]=(if(iloop==0) "i" else "")
+    if(dloop==1) fixedvalsmat[rowloop,3]=1-1e-8
+    if(iloop==1) fixedvalsmat[rowloop,4]=.0001
+    if(iloop==2) fixedvalsmat[rowloop,4]=1
+    if(rloop==1) fixedvalsmat[rowloop,1]=1
+    if(ploop==1) pequalsrmat[rowloop,1]=1
+  }
+  
+  parnames=apply(freeparsmat,1,paste,collapse="")
+  modnames=parnames
+  modnames[freeparsmat[,"i"]==""]=paste(modnames[freeparsmat[,"i"]==""], round(fixedvalsmat[freeparsmat[,"i"]=="","i"],1),sep="")
+  
+  
+twoLLstack <- array(NA, c(maxsubj, 1, 1))
+parstack <- array(NA, c(maxsubj, 4, 1))
+lengthvec=128-rowSums(rawdatamat[,1:128]==0)
 
-  
-  
-  for (subj in 1:maxsubj){
+for (subj in 1:maxsubj){
 
+  temppars <- runif(4)*ub
   
-  twoLLstack <- array(NA, c(maxsubj, 1, 1))
-  parstack <- array(NA, c(maxsubj, 4, 1))
-  setmod <- optim(temppars, MLEFUNTION, ... )
+  curlength=lengthvec[subj]
+  curmod <- 5
+  curchoices=data.frame(rawdatamat[subj,1:curlength])
+  curreinf=data.frame(rawdatamat[subj,129:(128+curlength)])
+  
+  temppars <- runif(4)*ub
+  setmod <- optim(temppars, vattG2overarchfun, freeletters=freeparsmat[curmod,],fixedvals=fixedvalsmat[curmod,],pequalsr=pequalsrmat[curmod,],
+                  tempchoices=curchoices,tempreinf=curreinf,predpfun=vattpredpfun9, lower=lb, upper=ub, method="L-BFGS-B")
+  
   
     for (iter in 1:maxiter){ 
       temppars <- runif(4)*ub
-      tempmod <- optim(temppars, MLEFUNCTION, ... )
+      tempmod <- optim(temppars, vattG2overarchfun, freeletters=freeparsmat[curmod,],fixedvals=fixedvalsmat[curmod,],pequalsr=pequalsrmat[curmod,],
+                       tempchoices=curchoices,tempreinf=curreinf,predpfun=vattpredpfun9, lower=lb, upper=ub, method="L-BFGS-B")
       
-      if (curmod$value < setmod$value){
+      if (tempmod$value < setmod$value){
         setmod <- tempmod
       }
       twoLLstack[subj,,] <- setmod$value
       parstack[subj,,] <- setmod$par
     }
     
-    
-  }
+}
   
+
+optim(temppars, vattG2overarchfun, freeletters=freeparsmat[curmod,],fixedvals=fixedvalsmat[curmod,],pequalsr=pequalsrmat[curmod,],
+                  tempchoices=curchoices,tempreinf=curreinf,predpfun=vattpredpfun9,lower=lb, upper=ub, method="L-BFGS-B") 
+
+optim(temppars, vattG2overarchfun, freeletters=freeparsmat[curmod,],fixedvals=fixedvalsmat[curmod,],pequalsr=pequalsrmat[curmod,],
+      tempchoices=curchoices,tempreinf=curreinf,predpfun=vattpredpfun9, method="Nelder-Mead") 
+
 k <-
 N <- #subj number?
 tBIC <- colSums(twoLLstack) + log(N)*k
--- probability.R
+++ probability.R
@@ -1,42 +1,40 @@
-
+#Creating 3*4*128 answer sheet from "correctdeck matrix"
+correctdeckmat=read.table("correctdeck matrix.txt",header=1) 
 matchstack=array(rep(0,(3*4*128)),dim=c(3,4,128)) 
-correctdeckmat=read.table("correctdeck matrix.txt",header=1)
-for (i in 1:4) matchstack[,i,]=array(as.numeric(correctdeckmat[,]==i),dim=c(3,128))
+  for (i in 1:4) matchstack[,i,] <- array(as.numeric(correctdeckmat[,]==i),dim=c(3,128))
 
+#Modulating for parameter bounds
 stretchpars=function(opars) -log((ub-lb)/(opars-lb)-1)	#opars=original pars
 contractpars=function(spars) (ub-lb)/(exp(-spars)+1)+lb		#spars=stretched pars
 
-scale3to2=function(temppars) c(temppars[1],temppars[2]/(1-temppars[1]))
-#this takes a vector of 3 p's that would sum to 1 and rescales them into 2 pars that range from 0-1
-#this is done so optimization with range 0-1 can work.  e.g. (.6,.1,.3) -> (.6,.25)
-
-scale2to3=function(temppars)  c(temppars[1],(1-temppars[1])*temppars[2],1-temppars[1]+(temppars[1]-1)*temppars[2])
-#this takes a vector of 2 pars that range from 0-1 and transforms back to 3 p's that sum to 1
-
+#Calculate attention(3) to dimensions: f
 powerrize=function(pow,tempvec) (tempvec^pow)/sum(tempvec^pow)
-#takes a vector, raises to a power, then rescales to sum to 1
 
-cattpredpfun=function(temppars,templength) 
-  #predicted probabilities assuming Constant Attention weights
-  #temppars is a 3 parameter pars vector representing the attention weights to color, form, and number
-{
-  tempmat=matrix(-1,ncol=templength,nrow=4)
-  for (temptrial in 1:templength) tempmat[,temptrial]=t(temppars%*%matchstack[,,temptrial])
-  tempmat
+#Calculate deck-probabilities(4) based on attention(3)
+cattpredp <- matrix(NA, ncol=128, nrow=4)
+  for(trial in 1:128){
+    cattpredp[,trial]=t(c(1/3,1/3,1/3)%*%matchstack[,,trial])
+  }
+
+cattG2fun=function(temppars,tempchoices){ #attention pars(3)
+  templength=length(tempchoices)
+  tempchoiceprob=matrix(c(tempchoices==1,tempchoices==2,tempchoices==3,tempchoices==4),nrow=4,ncol=templength,byrow=TRUE)*(cattpredp[,length(tempchoices)])
+  tempchoiceprob=colSums(tempchoiceprob)
+  tempchoiceprob=tempchoiceprob[2:templength]*.9998+.0001		#removes trial 1 and rescales. why?
+  return(-2*sum(log(tempchoiceprob)))
 }
 
-#vattpredpfun9 is an overarching all purpose model function calculating the predicted probability
-#takes parameters r, p, d, i ("i" might be called "g" or "f" in paper)
-#when trial>m the "aha" experience has occurred. globalm is a global variable
-#freeletters is a vector of characters in any order for the free parameter letters
-#fixedvals is an ordered vector numbers that only matters for nonfree parameters
-#pequalsr is boolean for whether constraint p=r is true
-vattpredpfun9=function(temppars,freeletters,fixedvals,pequalsr,tempchoices,tempreinf) 
-{
-  if(sum(freeletters=="r")>0) r=temppars[1] else r=fixedvals[1]
-  if(sum(freeletters=="p")>0) p=temppars[2] else p=fixedvals[2] 
-  if(sum(freeletters=="d")>0) d=temppars[3] else d=fixedvals[3]
-  if(sum(freeletters=="i")>0) i=temppars[4] else i=fixedvals[4]
+
+#Calculate deck-probability(4) under 24 models
+  #temppars = c(r,p,d,i)
+  #freeletters = c("i")... a vector of characters in any order for the free parameter letters
+  #fixedvals = an ordered vector numbers that only matters for nonfree parameters
+  #pequalsr = T/F for whether constraint p=r is true
+vattpredpfun9=function(temppars,freeletters,fixedvals,pequalsr,tempchoices,tempreinf){
+  if("r" %in% freeletters) r=temppars[1] else r=fixedvals[1]
+  if("p" %in% freeletters) p=temppars[2] else p=fixedvals[2] 
+  if("d" %in% freeletters) d=temppars[3] else d=fixedvals[3]
+  if("i" %in% freeletters) i=temppars[4] else i=fixedvals[4]
   if(pequalsr) p=r
   
   templength=length(tempchoices)
@@ -59,19 +57,10 @@ vattpredpfun9=function(temppars,freeletters,fixedvals,pequalsr,tempchoices,tempr
     }
     predpmat[,temptrial+1]=t(powerrize(d,curatt)%*%matchstack[,,temptrial+1])
   }
+  temppars <- c(r,p,d,i)
   return(predpmat)
 }
 
-cattG2fun=function(pars2,tempchoices) #generates G2 for constant attention
-  #this takes the 2 parameter pars vector (because only two are free)
-{
-  temppars=scale2to3(pars2)
-  templength=length(tempchoices)
-  tempchoiceprob=matrix(c(tempchoices==1,tempchoices==2,tempchoices==3,tempchoices==4),nrow=4,ncol=templength,byrow=TRUE)*(cattpredpfun(temppars,templength))
-  tempchoiceprob=colSums(tempchoiceprob)
-  tempchoiceprob=tempchoiceprob[2:templength]*.9998+.0001		#removes trial 1 and rescales
-  return(-2*sum(log(tempchoiceprob)))
-}
 
 vattG2overarchfun=function(temppars,tempparbounds,freeletters,fixedvals,pequalsr,tempchoices,tempreinf,predpfun) 
 {
